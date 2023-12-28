1c733980905e7cc198e204aa867868d8c3cc580c
Mykola Lazarchuk <nikolay.lazarchuk@gmail.com>
2023-12-28 12:22:31 +0200

    Create first test swiper slide

---
+++ Index.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: true, //свойство для центрирования слайдов
+    grabCursor: true, //курсор в виде руки
+    loop: true, // безконечное листание
+    navigation: { // кнопки влево-вправо
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    spaceBetween: 10, //gap
+    pagination: { 
+      el: ".swiper-pagination", //элемент HTML, который будет использоваться для отображения пагинации.
+      clickable: true, // позволяет кли кать по точкам/индикаторам пагинации
+    },
+  });
